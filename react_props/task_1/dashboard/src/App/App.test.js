f05851335c86b6176f7dc50c8f891e0208f2a7a1
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-08-27 19:10:29 +0200

    app test

--- react_props/task_1/dashboard/src/App/App.test.js
+++ react_props/task_1/dashboard/src/App/App.test.js
@@ -7,19 +7,24 @@ describe("<App />", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toEqual(true);
   });
-  // it("App renders a 'div' with the class App-header", () => {
-  //   const wrapper = shallow(<App />);
-  //   wrapper.update();
-  //   expect(wrapper.find("div.App-header")).toHaveLength(1);
-  // });
-  // it("App renders a div with the class App-body", () => {
-  //   const wrapper = shallow(<App />);
-  //   wrapper.update();
-  //   expect(wrapper.find("div.App-body")).toHaveLength(1);
-  // });
-  // it("App renders a div with the class App-footer", () => {
-  //   const wrapper = shallow(<App />);
-  //   wrapper.update();
-  //   expect(wrapper.find("div.App-footer")).toHaveLength(1);
-  // });
+  it("should contain the Notifications component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Notifications")).toHaveLength(1);
+  });
+  it("should contain the Header component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Header")).toHaveLength(1);
+  });
+  it("should contain the Login component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Login")).toHaveLength(1);
+  });
+  it("should contain the Footer component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Footer")).toHaveLength(1);
+  });
 });
